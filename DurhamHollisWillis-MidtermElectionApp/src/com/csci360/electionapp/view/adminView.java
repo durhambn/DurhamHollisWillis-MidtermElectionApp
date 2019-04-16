377087b63cb00fdc583b1ef5ddf4335643bd2a53
Play3rJu5t1n <willisjk@g.cofc.edu>
2019-04-16 17:59:06 -0400

    main and adminView changes

--- DurhamHollisWillis-MidtermElectionApp/src/com/csci360/electionapp/view/adminView.java
+++ DurhamHollisWillis-MidtermElectionApp/src/com/csci360/electionapp/view/adminView.java
@@ -174,7 +174,7 @@ public class adminView {
 		alert.initOwner(reTally.getScene().getWindow());
 		alert.setTitle("ADD TESTER");
 		alert.setHeaderText("Added Tester User");
-		alert.setContentText("The tester's username is: testUser \npassword: Test123*");
+		alert.setContentText("The tester's username is: testUser \npassword: TestUser123*");
 		alert.showAndWait();
 	}
 	public void deleteTest(ActionEvent event) throws SQLException, IOException {
